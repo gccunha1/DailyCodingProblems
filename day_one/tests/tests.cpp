5ffa3a2129c77d8739757975518c9975edf87ea1
gccunha1 <goncalocarvalhocunha@gmail.com>
2021-11-16 22:35:44 +0000

    Added new tests

--- day_one/tests/tests.cpp
+++ day_one/tests/tests.cpp
@@ -7,6 +7,41 @@ TEST(DayOne, NoSolution)
     std::vector<int> input = {1, 1, 1};
     int sum = 30;
     EXPECT_FALSE(check_if_sum_of_two_numbers_in_vector(input, sum));
+    EXPECT_FALSE(check_if_sum_of_two_numbers_in_vector_positives(input, sum));
+}
+
+TEST(DayOne, GivenVectorWithSolutionExpectSolutionFound)
+{
+    //create vector of ints
+    std::vector<int> input = {1, 1, 37};
+    int sum = 2;
+    EXPECT_TRUE(check_if_sum_of_two_numbers_in_vector(input, sum));
+    EXPECT_TRUE(check_if_sum_of_two_numbers_in_vector_positives(input, sum));
+}
+
+TEST(DayOne, GivenVectorWithSolutionAddingZeroExpectSolutionFound)
+{
+    //create vector of ints
+    std::vector<int> input = {1, 1, 37, 0};
+    int sum = 37;
+    EXPECT_TRUE(check_if_sum_of_two_numbers_in_vector(input, sum));
+    EXPECT_TRUE(check_if_sum_of_two_numbers_in_vector_positives(input, sum));
+}
+
+TEST(DayOne, SolutionSumZero)
+{
+    //create vector of ints
+    std::vector<int> input = {1, 2, -2};
+    int sum = 0;
+    EXPECT_TRUE(check_if_sum_of_two_numbers_in_vector(input, sum));
+}
+
+TEST(DayOne, SolutionSumNegative)
+{
+    //create vector of ints
+    std::vector<int> input = {1, 2, -2};
+    int sum = -1;
+    EXPECT_TRUE(check_if_sum_of_two_numbers_in_vector(input, sum));
 }
 
 int main(int argc, char **argv)
