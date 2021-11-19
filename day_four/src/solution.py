46c4b8d9fd684058e60fd7761b44526aa119230e
gccunha1 <goncalocarvalhocunha@gmail.com>
2021-11-19 22:51:58 +0000

    Improved first solution

--- day_four/src/solution.py
+++ day_four/src/solution.py
@@ -1,15 +1,14 @@
 def solution(integer_array):
-    maximum = max(integer_array)
-    occurences_list = [0] * maximum
+    occurrences = [0] * len(integer_array)
     for value in integer_array:
-        if value >= 0:
-            occurences_list[value - 1] = 1
+        if value > 0 and value <= len(occurrences):
+            occurrences[value - 1] = 1
 
     try:
-        return occurences_list.index(0) + 1
+        return occurrences.index(0) + 1
     except ValueError:
-        return maximum + 1
+        return len(integer_array) + 1
 
 
 if __name__ == "__main__":
-    solution([7, 9, 6, 4, 8, 1, 6, 2, 0, 54, 7, 5, -2])
+    print(solution([7, 9, 6, 4, 8, 1, 6, 2, 0, 54, 7, 5, -2]))
--- day_four/src/test.py
+++ day_four/src/test.py
@@ -1,5 +1,6 @@
 import unittest
 import solution
+import random
 
 
 class TestSolution(unittest.TestCase):
@@ -10,6 +11,19 @@ class TestSolution(unittest.TestCase):
     def test_example_tests(self):
         self.expected_output_test([3, 4, -1, 1], 2)
         self.expected_output_test([1, 2, 0], 3)
+        self.expected_output_test([5, 6, 7, 8, 4, 3, 1, 2], 9)
+
+    def test_random_list(self):
+        random_list = [random.randrange(1, 10000, 1) for i in range(1000)]
+        sorted_list = list(dict.fromkeys(sorted(random_list)))
+
+        answer = 1
+        for idx, value in enumerate(sorted_list):
+            if idx + 1 != value:
+                break
+            answer += 1
+
+        self.expected_output_test(random_list, answer)
 
 
 if __name__ == "__main__":
