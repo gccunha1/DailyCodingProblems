e3555e34a67d7db6750da2c6d62541b04b2a6f28
gccunha1 <goncalocarvalhocunha@gmail.com>
2021-11-21 23:03:32 +0000

    Add solution passing tests

--- day_six/src/solution.h
+++ day_six/src/solution.h
@@ -1,6 +1,9 @@
 #ifndef SOLUTION_H
 #define SOLUTION_H
 
+#include <ostream>
+#include <iostream>
+
 template <class T>
 class EfficientDoubleLinkedList
 {
@@ -9,23 +12,85 @@ private:
     {
         T val;
         Node *both;
+        Node(T new_val) : val{new_val}, both{nullptr} {}
     };
-    Node *head;
 
-    Node *GetNextNode(Node *current_node);
-    Node *GetPreviousNode(Node *current_node);
+    Node *head;
+    Node *tail;
+    int size;
+
+    Node *GetNextNode(Node *current_node, Node *previous_node = nullptr)
+    {
+        if (current_node == nullptr)
+            return nullptr;
+        return (Node *)((uint64_t)current_node->both ^ (uint64_t)previous_node);
+    }
 
 public:
-    EfficientDoubleLinkedList() : head{nullptr}
+    EfficientDoubleLinkedList() : head{nullptr}, tail{nullptr}, size{0}
     {
     }
     ~EfficientDoubleLinkedList()
     {
+        Node *current_node = head, *previous_node = nullptr, *buffer_pointer = nullptr;
+
         //Loop through list
-        //free memory
+        while (current_node != nullptr)
+        {
+            //free memory
+            buffer_pointer = current_node;
+            current_node = GetNextNode(current_node, previous_node);
+            previous_node = buffer_pointer;
+            delete previous_node;
+        }
     }
-    void Add(T element);
-    T Get(uint8_t index);
+    void Add(T element)
+    {
+        Node *new_node = new Node(element);
+        if (new_node == nullptr)
+        {
+            std::cout << "Memory error" << std::endl;
+            exit(1);
+        }
+        size++;
+        if (tail == nullptr)
+        {
+            head = new_node;
+            tail = new_node;
+            return;
+        }
+        tail->both = (Node *)((uint64_t)tail->both ^ (uint64_t)new_node);
+        new_node->both = (Node *)((uint64_t)new_node->both ^ (uint64_t)tail);
+        tail = new_node;
+    }
+    T Get(int index)
+    {
+        if (index < 0 || index >= size)
+        {
+            std::cout << "Index out of bounds" << std::endl;
+            exit(1);
+        }
+        Node *current_node = nullptr, *previous_node = nullptr, *buffer_pointer = nullptr;
+        int start_index;
+        if (index < size / 2)
+        {
+            current_node = head;
+            start_index = 0;
+        }
+        else
+        {
+            current_node = tail;
+            start_index = size - 1;
+        }
+
+        for (int i = 0; i < abs(index - start_index); i++)
+        {
+            buffer_pointer = current_node;
+            current_node = GetNextNode(current_node, previous_node);
+            previous_node = buffer_pointer;
+        }
+        return current_node->val;
+    }
 };
 
 #endif
